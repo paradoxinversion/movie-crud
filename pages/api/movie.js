7932f821e7428fe1d4b79f7995bf5be03d872120
Jedai Saboteur <paradox.inversion@gmail.com>
2021-06-18 11:44:10 -0700

    Implemented PUT/update on movie route

--- pages/api/movie.js
+++ pages/api/movie.js
@@ -27,6 +27,19 @@ export default async (req, res) => {
         res.status(200).json({ msg: "Movie Saved!" });
       }
       case "PUT": {
+        const { id } = req.body;
+        const updateFields = req.body.updateMovieFields;
+        if (!id || !updateFields) {
+          throw new Error(
+            "ID and/or Update fields are missing from request body. Please modify your request and try again."
+          );
+        }
+        const movieUpdate = await Movie.findByIdAndUpdate(id, updateFields, {
+          new: true,
+          lean: true,
+          runValidators: true,
+        });
+        res.status(200).json({ msg: "Movie Updated!", movie: movieUpdate });
         break;
       }
       case "DELETE": {
